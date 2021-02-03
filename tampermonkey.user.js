f5f0699024e68dfa369334a90da75e28be6dcd5b
Genius6942 <74061612+Genius6942@users.noreply.github.com>
2021-02-03 15:21:50 -0500

    Add files via upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         UpVSDown
+// @namespace    http://tampermonkey.net/
+// @version      0.5
+// @description  try to take over the world!
+// @author       Adventure10
+// @match        https://artofproblemsolving.com/*
+// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('in progress...')
+        // Your code here...
+})();
